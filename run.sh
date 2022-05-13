f886875bdaa9b74cc2b5a44671f946479be54c92
Dam Heo <50097909+lk5925@users.noreply.github.com>
2022-05-13 20:16:51 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run_clm.py \
---model_name_or_path distilgpt2 \
+CUDA_VISIBLE_DEVICES=0 python run_clm.py \
+--model_name_or_path gpt2 \
 --run_name wechsel-gpt2-ko \
---dataset_name oscar \
---dataset_config_name unshuffled_deduplicated_ko \
+--train_file ./kcbert-data/kcbert-data.txt \
 --output_dir ./wechsel-gpt2-ko \
+--evaluation_strategy steps \
+--overwrite_output_dir \
+--eval_steps 1000 \
 --save_steps 1000 \
 --save_total_limit 10 \
 --report_to all \
 --logging_first_step \
---logging_steps 500 \
+--logging_steps 1000 \
 --do_train \
 --do_eval \
 --validation_split_percentage 5 \
@@ -16,5 +18,5 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python run_clm.py \
 --metric_for_best_model ppl \
 --fp16 true \
 --num_train_epochs 200 \
---gradient_accumulation_steps 2 \
+--gradient_accumulation_steps 1 \
 --per_device_train_batch_size 16
